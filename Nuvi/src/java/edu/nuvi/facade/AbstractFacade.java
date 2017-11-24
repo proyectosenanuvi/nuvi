8997db067684788f0bbd7847353c7956eba5266c
Enrique <Enrique@LAPTOP-G8A8TQM0>
2017-11-23 21:08:17 -0500

    Actualizacion facade

--- Nuvi/src/java/edu/nuvi/facade/AbstractFacade.java
+++ Nuvi/src/java/edu/nuvi/facade/AbstractFacade.java
@@ -1,12 +1,12 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor..
  */
 package edu.nuvi.facade;
 
 import java.util.List;
-import javax.persistence.EntityManager;
+import javax.persistence.EntityManager; 
 
 /**
  *
